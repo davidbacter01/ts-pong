ff88a54e736c541a15f6dc67f84fc31d0c282790
davidbacter01 <davidbacter01@gmail.com>
2022-08-17 01:41:49 +0300

    Demo draw a paddle

--- src/index.ts
+++ src/index.ts
@@ -1,1 +1,9 @@
-console.log('hello from ts!');
+const canvas: HTMLCanvasElement = document.getElementById('gameScreen') as undefined as HTMLCanvasElement;
+canvas.width = window.innerWidth;
+canvas.height = window.innerHeight;
+canvas.style.backgroundColor = 'black';
+
+const ctx = canvas.getContext('2d');
+
+ctx.fillStyle = 'red';
+ctx.fillRect(100, 100, 40, 200);

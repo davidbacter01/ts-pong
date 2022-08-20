ae19ef4dba4f8ae2c23f490e122ef8ae68aa92a1
davidbacter01 <davidbacter01@gmail.com>
2022-08-20 16:04:11 +0300

    Adds css and minor style reset

--- src/index.ts
+++ src/index.ts
@@ -1,6 +1,6 @@
 const canvas: HTMLCanvasElement = document.getElementById('gameScreen') as undefined as HTMLCanvasElement;
 canvas.width = window.innerWidth;
-canvas.height = window.innerHeight;
+canvas.height = window.innerHeight - 4;
 canvas.style.backgroundColor = 'black';
 
 const ctx = canvas.getContext('2d');
